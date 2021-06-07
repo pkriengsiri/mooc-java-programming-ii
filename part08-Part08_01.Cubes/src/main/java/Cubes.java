a38777ea7fae25231aa6f2b5a403f3156e1132db
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-06-07 19:56:43 -0400

    finished through 8.2

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,18 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while(true) {
+            String input = scanner.nextLine();
+            
+            if(input.equals("end")) {
+                break;
+            }
+            
+            int inputInteger = (int) Math.pow(Integer.valueOf(input), 3);
+            
+            System.out.println(inputInteger);
+        }
 
     }
 }
--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,44 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        processInput(scanner);
 
+    }
+
+    public static void processInput(Scanner scanner) {
+        int count = 0;
+        int sum = 0;
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.equals("0")) {
+                break;
+            }
+
+            int inputInteger = Integer.valueOf(input);
+
+            if (isPositive(inputInteger)) {
+                count++;
+                sum += inputInteger;
+            }
+
+        }
+
+        if (count == 0) {
+            System.out.println("Cannot calculate the average");
+
+        } else {
+            System.out.println((double) sum / count);
+        }
+    }
+
+    public static Boolean isPositive(int number) {
+        if (number > 0) {
+            return true;
+        }
+        return false;
     }
 }
